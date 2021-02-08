bad0d6a304ddbb6e17f9b45dda554f74ac47dbec
Баранова Анастасия <abaranova2002@gmail.com>
2021-02-08 16:59:21 +0300

    04task fixed

--- 04task.c
+++ 04task.c
@@ -14,6 +14,8 @@ int main(int argc, char *argv[], char *envp[])
 	} else if (pid == 0) {
 		printf("I'm a child. Look at this:\n");
 		(void) execle("/bin/cat", "/bin/cat", "03task.c", 0, envp);
+		printf("Error on program start.\n");
+		exit(-1);
 	} else {
 		printf("I'm a parent\n");
 	}
