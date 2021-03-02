90969df520584535cc385998622a4c1ffa69c5cd
Баранова Анастасия <abaranova2002@gmail.com>
2021-03-02 12:43:59 +0300

    something changed

--- 01task_reader.c
+++ 01task_reader.c
@@ -6,46 +6,43 @@
 #include <stdlib.h>
 
 int main() {
-	char* text;
-	int size;
-	int* ptr;
+   	char* text;
+   	int size = 0;
+   	int* ptr;
  
-	int     shmid;
-	char    pathname[] = "01task_writer.c";
-	key_t   key;
-  	int     new = 1;
+   	int     shmid;
+   	char    pathname[] = "01task_writer.c";
+   	key_t   key;
  
-	if ((key = ftok(pathname, 0)) < 0) {
-		printf("Can\'t generate key\n");
-		exit(-1);
-	}
-
-	if ((shmid = shmget(key, sizeof(int) + size * sizeof(char), 0)) < 0) {
-		printf("Can\'t find shared memory\n");
+   	if ((key = ftok(pathname,0)) < 0) {
+        	printf("Can\'t generate key\n");
         	exit(-1);
-	}
-
-	if ((ptr = (int *)shmat(shmid, NULL, 0)) == (int *)(-1)) {
-		printf("Can't attach shared memory\n");
-		exit(-1);
-	}
-
-	size = *ptr;
-	text = (char*)(ptr + 1);
-
-	for (int i = 0; i < size; ++i) {
-		putchar(text[i]);
-	}
-	
-	if (shmdt(ptr) < 0) {
-		printf("Can't detach shared memory\n");
-		exit(-1);
-	}
-
-	if(shmctl(shmid, IPC_RMID, NULL) < 0) {
-		printf("Can't delete shared memory\n");
-		exit(-1);
-	}
-
-	return 0;
+    	}
+    
+    	if ((shmid = shmget(key, sizeof(int) + size * sizeof(char), 0)) < 0) {
+        	printf("Can\'t create shared memory\n");
+        	exit(-1);
+    	}
+    	if ((ptr = (int *)shmat(shmid, NULL, 0)) == (int *)(-1)) {
+        	printf("Can't attach shared memory\n");
+        	exit(-1);
+    	}
+    
+    	size = *ptr;
+    	text = (char*)(ptr + 1);
+    
+    	for (int i = 0; i < size; i++)
+        	putchar(text[i]);
+    
+    	if (shmdt(ptr) < 0) {
+        	printf("Can't detach shared memory\n");
+        	exit(-1);
+    	}
+    
+    	if (shmctl(shmid, IPC_RMID, NULL) < 0) {
+        	printf("Can't delete shared memory\n");
+        	exit(-1);
+    	}
+    
+    	return 0;
 }
