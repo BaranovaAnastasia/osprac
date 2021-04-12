3cdf4970d079b62d709eeb967147f291b6ca43b5
Анастасия Баранова <aabaranova_3@edu.hse.ru>
2021-04-12 20:26:24 +0300

    fixed 2

--- 07-3b.c
+++ 07-3b.c
@@ -34,23 +34,37 @@ int main()
         }
     }
 
+    if ((semid = semget(key, 1, 0666)) < 0) {
+        if (errno != EEXIST) {
+            printf("Can\'t create semaphore\n");
+            exit(-1);
+        } else {
+            if ((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0) {
+                printf("Can\'t find semaphore\n");
+                exit(-1);
+            }
+        }
+    } else {
+        mybuf.sem_num = 0;
+        mybuf.sem_op  = 1;
+        mybuf.sem_flg = 0;
+        if (semop(semid, &mybuf, 1) < 0) {
+            printf("Can\'t add 1 to semaphore\n");
+            exit(-1);
+        }
+    }
+
     if ((array = (int *)shmat(shmid, NULL, 0)) == (int *)(-1)) {
         printf("Can't attach shared memory\n");
         exit(-1);
     }
 
-    if ((semid = semget(key, 1, 0666)) < 0) {
-        printf("Semaphore not found.\n");
-        if ((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0) {
-            printf("Can\'t get semid\n");
-            exit(-1);
-        }
-        printf("Created successfully\n");
-
-        mybuf.sem_num = 0;
-        mybuf.sem_op = 1;
-        mybuf.sem_flg = 0;
-        semop(semid, buf, 1);
+    mybuf.sem_num = 0;
+    mybuf.sem_op  = -1;
+    mybuf.sem_flg = 0;
+    if (semop(semid, &mybuf, 1) < 0) {
+        printf("Can\'t wait for semaphore\n");
+        exit(-1);
     }
 
     if (new) {
@@ -58,22 +72,22 @@ int main()
         array[1] =  1;
         array[2] =  1;
     } else {
-        mybuf.sem_num = 0;
-        mybuf.sem_op = -1;
-        mybuf.sem_flg = 0;
-        semop(semid, buf, 1);
         array[1] += 1;
         for (i = 0; i < 2000000000L; i++);
         array[2] += 1;
-        mybuf.sem_num = 0;
-        mybuf.sem_op = 1;
-        mybuf.sem_flg = 0;
-        semop(semid, buf, 1);
     }
 
     printf
         ("Program 1 was spawn %d times, program 2 - %d times, total - %d times\n",
         array[0], array[1], array[2]);
+
+    mybuf.sem_num = 0;
+    mybuf.sem_op  = 1;
+    mybuf.sem_flg = 0;
+    if (semop(semid, &mybuf, 1) < 0) {
+        printf("Can\'t add 1 to semaphore\n");
+        exit(-1);
+    }
 
     if (shmdt(array) < 0) {
         printf("Can't detach shared memory\n");
