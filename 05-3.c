aed49f02ff2c7c3c2982de590dcdb0c576414573
Баранова Анастасия <abaranova2002@gmail.com>
2021-04-19 14:25:39 +0300

    fix

--- 05-3.c
+++ 05-3.c
@@ -51,23 +51,20 @@ int main()
         for (int i = 0; i < n; ++i) {
             size = write(fd[1], "Hello, world!", 14);
 
+	   
+
             if (size != 14) {
-                printf("Can\'t write all string to pipe\n");
+                printf("Parent can\'t write all string to pipe\n");
                 exit(-1);
             } else {
                 printf("Parent wrote their message.\n");
-            }
-
-            if (close(fd[1]) < 0) {
-                printf("parent: Can\'t close writing side of pipe\n");
-                exit(-1);
             }
 
             mybuf.sem_op = -1;
             mybuf.sem_flg = 0;
             mybuf.sem_num = 0;
 
-            semop(semid, mybuf, 1);
+            semop(semid, &mybuf, 1);
 
             size = read(fd[0], resstring, 14);
             if (size < 0) {
@@ -77,33 +74,27 @@ int main()
 
             printf("Parent read: %s\n", resstring);
 
-            if (close(fd[0]) < 0) {
+            printf("Parent exit\n");
+        }
+	if (close(fd[1]) < 0) {
+                printf("parent: Can\'t close writing side of pipe\n");
+                exit(-1);
+            }
+	    if (close(fd[0]) < 0) {
                 printf("parent: Can\'t close reading side of pipe\n");
                 exit(-1);
             }
-
-            printf("Parent exit\n");
-        }
     } else {
-
         /* Child process */
         for (int i = 0; i < n; ++i){
+	    
+
             size = read(fd[0], resstring, 14);
             if (size < 0) {
-                printf("Can\'t read string\n");
+                printf("Child can\'t read string\n");
                 exit(-1);
             }
             printf("Child read: %s\n", resstring);
-
-            if (close(fd[0]) < 0) {
-                printf("child: Can\'t close reading side of pipe\n"); exit(-1);
-            }
-
-            for (int i = 0; i < 13 / 2; i++) {
-                char t = resstring[i];
-                resstring[i] = resstring[13 - i - 1];
-                resstring[13 - i - 1] = t;
-            }
 
             size = write(fd[1], resstring, 14);
 
@@ -111,7 +102,7 @@ int main()
             mybuf.sem_flg = 0;
             mybuf.sem_num = 0;
 
-            semop(semid, mybuf, 1);
+            semop(semid, &mybuf, 1);
 
             if (size != 14) {
                 printf("Child can\'t write all string\n");
@@ -119,12 +110,13 @@ int main()
             } else {
                 printf("Child wrote their message.\n");
             }
-
-            if (close(fd[1]) < 0) {
-                printf("child: Can\'t close writing side of pipe\n"); exit(-1);
-            }
-
             printf("Child exit.\n");
+        }
+	if (close(fd[1]) < 0) {
+                printf("child: Can\'t close writing side of pipe\n"); exit(-1);
+        }
+	if (close(fd[0]) < 0) {
+                printf("child: Can\'t close reading side of pipe\n"); exit(-1);
         }
     }
 
